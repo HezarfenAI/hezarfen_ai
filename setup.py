b422cf95db5376e5409c39ff59d19327cc170107
Ömer Asaf Karasu <omerasafkarasu06@gmail.com>
2025-04-15 18:40:23 +0300

    Remove CLI script and update package version to 0.0.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding = "utf-8") as fh:
 
 setuptools.setup(
     name = "hezarfenai",
-    version = "0.0.6",
+    version = "0.0.7",
     author = "Ömer Asaf Karasu",
     author_email = "omer.karasu@niafix.com",
     description = "A Teknofest project named HezarfenAI Official Python Package",
